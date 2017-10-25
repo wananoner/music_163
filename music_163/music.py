463027ed7b29bb7c9bea1a0ca10e29a95a19d427
wananoner <wananoner@gmail.com>
2017-10-25 23:48:03 +0800

    重写

--- music_163/music.py
+++ music_163/music.py
@@ -2,57 +2,49 @@ import requests
 import time
 import pymysql
 from lxml import etree
+from urllib.parse import urljoin
 
 db = pymysql.connect(host="127.0.0.1", user="root", password="password", db="music163", charset="utf8")
 cursor = db.cursor()
 
 
-def name_href():
-    names = []
-    hrefs = []
-    urls = ['http://music.163.com/discover/artist/cat?id=1001', 'http://music.163.com/discover/artist/cat?id=1002',
-            'http://music.163.com/discover/artist/cat?id=1003', 'http://music.163.com/discover/artist/cat?id=2001',
-            'http://music.163.com/discover/artist/cat?id=2002', 'http://music.163.com/discover/artist/cat?id=2003',
-            'http://music.163.com/discover/artist/cat?id=6001', 'http://music.163.com/discover/artist/cat?id=6002',
-            'http://music.163.com/discover/artist/cat?id=6003', 'http://music.163.com/discover/artist/cat?id=7001',
-            'http://music.163.com/discover/artist/cat?id=7002', 'http://music.163.com/discover/artist/cat?id=7003',
-            'http://music.163.com/discover/artist/cat?id=4001', 'http://music.163.com/discover/artist/cat?id=4002',
-            'http://music.163.com/discover/artist/cat?id=4003']
-    header = {
+def open_url(url):
+    headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-                        Chrome/61.0.3163.79 Safari/537.36'}
-    try:
-        for url in urls:
-            for i in range(65, 91):
-                url_name = url + '&initial=' + str(i)
-                req = requests.get(url_name, headers=header)
-                selector = etree.HTML(req.text)
-                name = selector.xpath('//ul[@id="m-artist-box"]/li/p/a[1]/text()|\
-                                       //ul[@id="m-artist-box"]/li/a[1]/text()')
-                href = selector.xpath('//ul[@id="m-artist-box"]/li/p/a[1]/@href|\
-                                       //ul[@id="m-artist-box"]/li/a[1]/@href')
-                [names.append(i) for i in name]
-                [hrefs.append(i) for i in href]
-            url_name = url + '&initial=0'
-            req = requests.get(url_name, headers=header)
-            selector = etree.HTML(req.text)
-            print(url_name)
-            name = selector.xpath('//ul[@id="m-artist-box"]/li/p/a[1]/text()|\
-                                   //ul[@id="m-artist-box"]/li/a[1]/text()')
-            href = selector.xpath('//ul[@id="m-artist-box"]/li/p/a[1]/@href|\
-                                   //ul[@id="m-artist-box"]/li/a[1]/@href')
-            [names.append(i) for i in name]
-            [hrefs.append(i) for i in href]
-            time.sleep(10)
+                            Chrome/61.0.3163.79 Safari/537.36'}
+    req = requests.get(url, headers)
+    req.encoding = 'utf-8'
+    selector = etree.HTML(req.text)
 
-    except Exception as e:
-        print(str(e))
-    print(names)
-    print(hrefs)
-    for (name, href) in zip(names, hrefs):
-        sql_insert = "INSERT INTO songer(name, name_href) VALUES (%s, %s)"
-        cursor.execute(sql_insert, (name, href))
-        db.commit()
+    return selector
 
 
-name_href()
+def songer():
+    url = 'http://music.163.com'
+    selector = open_url('http://music.163.com/discover/artist')
+    leixing_href = selector.xpath('//div[@class="blk"]/ul/li/a/@href')
+    leixing_name = selector.xpath('//div[@class="blk"]/ul/li/a/text()')
+    for (href, leixing) in zip(leixing_href, leixing_name):
+        print('获取：', leixing)
+        url_word = urljoin(url, href)
+        word_list = open_url(url_word).xpath('//ul[@id="initial-selector"]/li/a/@href')[1:]
+        for words in word_list:
+            url_name = urljoin(url, words)
+            songer_name = open_url(url_name).xpath('//ul[@id="m-artist-box"]/li/p/a/text()|\
+                                                    //ul[@id="m-artist-box"]/li/a/text()')
+            songer_url = [urljoin(url, href[1:]) for href in open_url(url_name).xpath(
+                        '//ul[@id="m-artist-box"]/li/p/a[1]/@href|//ul[@id="m-artist-box"]/li/a[1]/@href')]
+            # 数据库插入、字段 songer_name, songer_url, leixing
+            for (name, url) in zip(songer_name, songer_url):
+                sql_insert = "INSERT INTO songer(songer_name, songer_url, songer_leixing) VALUES (%s, %s, %s)"
+                cursor.execute(sql_insert, (name, url, leixing))
+                db.commit()
+        time.sleep(10)
+
+
+def main():
+    songer()
+
+
+if __name__ == '__main__':
+    main()
