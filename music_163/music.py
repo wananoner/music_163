6ea59224f1b1e24c63c1cf71d594c32898bc02ca
wangbo <wanan@risksyun.com>
2018-08-09 11:10:37 +0800

    change code

--- music_163/music.py
+++ music_163/music.py
@@ -3,49 +3,52 @@ import time
 import pymysql
 from lxml import etree
 from urllib.parse import urljoin
-
-db = pymysql.connect(host="127.0.0.1", user="root", password="password", db="music163", charset="utf8")
-cursor = db.cursor()
+from fake_useragent import UserAgent
 
 
-def open_url(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-                            Chrome/61.0.3163.79 Safari/537.36'}
-    req = requests.get(url, headers)
-    req.encoding = 'utf-8'
-    selector = etree.HTML(req.text)
+class MusicDB:
+    def __init__(self):
+        self.db = pymysql.connect(host="127.0.0.1", user="root", password="password", db="music163", charset="utf8")
+        self.cursor = self.db.cursor()
 
-    return selector
-
-
-def songer():
-    """歌手"""
-    url = 'http://music.163.com'
-    selector = open_url('http://music.163.com/discover/artist')
-    leixing_href = selector.xpath('//div[@class="blk"]/ul/li/a/@href')
-    leixing_name = selector.xpath('//div[@class="blk"]/ul/li/a/text()')
-    for (href, leixing) in zip(leixing_href, leixing_name):
-        print('获取：', leixing)
-        url_word = urljoin(url, href)
-        word_list = open_url(url_word).xpath('//ul[@id="initial-selector"]/li/a/@href')[1:]
-        for words in word_list:
-            url_name = urljoin(url, words)
-            songer_name = open_url(url_name).xpath('//ul[@id="m-artist-box"]/li/p/a/text()|\
-                                                    //ul[@id="m-artist-box"]/li/a/text()')
-            songer_url = [urljoin(url, href[1:]) for href in open_url(url_name).xpath(
-                        '//ul[@id="m-artist-box"]/li/p/a[1]/@href|//ul[@id="m-artist-box"]/li/a[1]/@href')]
-            # 数据库插入、字段 songer_name, songer_url, leixing
-            for (name, url) in zip(songer_name, songer_url):
-                sql_insert = "INSERT INTO songer(songer_name, songer_url, songer_leixing) VALUES (%s, %s, %s)"
-                cursor.execute(sql_insert, (name, url, leixing))
-                db.commit()
-        time.sleep(10)
+    def insert_db(self, name, url, singer_type):
+        sql_insert = "INSERT INTO singer(name, url, singer_type) VALUES (%s, %s, %s)"
+        self.cursor.execute(sql_insert, (name, url, singer_type))
+        self.db.commit()
 
 
-def main():
-    songer()
+class MusicSpider:
+    def __init__(self):
+        self.headers = {'User-Agent': UserAgent().random}
+
+    def get_url(self, url):
+        response = requests.get(url, self.headers)
+        response.encoding = 'utf-8'
+        selector = etree.HTML(response.text)
+        return selector
+
+    def singer(self):
+        """歌手"""
+        md = MusicDB()
+        url = 'http://music.163.com'
+        selector = self.get_url('http://music.163.com/discover/artist')
+        singer_type_href = selector.xpath('//div[@class="blk"]/ul/li/a/@href')
+        singer_type_name = selector.xpath('//div[@class="blk"]/ul/li/a/text()')
+        for (href, type_name) in zip(singer_type_href, singer_type_name):
+            print('获取：', type_name)
+            url_word = urljoin(url, href)
+            word_list = self.get_url(url_word).xpath('//ul[@id="initial-selector"]/li/a/@href')[1:]
+            for words in word_list:
+                url_name = urljoin(url, words)
+                songer_name = self.get_url(url_name).xpath('//ul[@id="m-artist-box"]/li/p/a/text()|\
+                                                        //ul[@id="m-artist-box"]/li/a/text()')
+                songer_url = [urljoin(url, href[1:]) for href in self.get_url(url_name).xpath(
+                    '//ul[@id="m-artist-box"]/li/p/a[1]/@href|//ul[@id="m-artist-box"]/li/a[1]/@href')]
+                for (name, url) in zip(songer_name, songer_url):
+                    md.insert_db(name, url, type_name)
+            time.sleep(10)
 
 
 if __name__ == '__main__':
-    main()
+    ms = MusicSpider()
+    ms.singer()
